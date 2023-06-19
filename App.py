572aa5e3e5ace3e64a589da18066f32cc565c640
reubermax <max.bobek@gmail.com>
2023-06-19 16:58:22 +0200

    changed squares to olive

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ class ChessBoard(ttk.Frame):
     def __init__(self):
         super().__init__()
         self.squares = [[None] * 8] * 8  # [0][0] is square A1 -> [7][7] = H8
-        self.colors = ("black", "white")
+        self.colors = ("#598c00", "white")
         self.setup_board()
 
 
@@ -22,8 +22,12 @@ class ChessBoard(ttk.Frame):
         square = tk.PhotoImage(width=1, height=1)
         for i in range(8):
             for j in range(8):
-                self.squares[i][j] = tk.Button(self, image=square, width=90, height=90, background=self.colors[(i+j)%2])
+                self.squares[i][j] = tk.Button(self, image=square, width=90, height=90, background=self.colors[(i+j)%2], command="self.change_square(self, i, j)")
                 self.squares[i][j].grid(row=i, column=j)
+
+    def change_square(self, i, j):
+        square = tk.Image(width=1, height=1, file="/pieces/png/b_bishop.png", imgtype="png")
+        self.squares[i][j] = tk.Button(self, image=square)
 
 if __name__ == "__main__":
     app = App()
