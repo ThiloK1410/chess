1822b62fd64ae34e1a84f69c2f0231628be92ffc
reubermax <max.bobek@gmail.com>
2023-06-29 16:35:54 +0200

    implemented window with title and icon in pyqt6, added ico file

--- src/GUI.py
+++ src/GUI.py
@@ -1,5 +1,18 @@
-from PyQt6 import QtGui
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QTextEdit, QVBoxLayout
+from PyQt6.QtGui import QIcon
 
-class GUI:
+class MyApp(QWidget):
     def __init__(self):
-        pass
+        super().__init__()
+        self.setWindowTitle("Chess Configurator")
+        self.setWindowIcon(QIcon('../media/icons/knight_icon.ico'))
+        self.resize(500, 300) # width, height
+
+app = QApplication(sys.argv)
+
+window = MyApp()
+window.show()
+
+app.exec()
+
